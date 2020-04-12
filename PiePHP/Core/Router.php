7ac0119d3dc2903cea71ec44b3583f8a40d16d52
Victor Reyes <victor.reyes@hotmail.fr>
2020-04-12 17:32:11 +0200

    Cleaning

--- PiePHP/Core/Router.php
+++ PiePHP/Core/Router.php
@@ -4,12 +4,10 @@ namespace Core;
 
 class Router
 {
-  //register -> user, add
   private static $routes;
   static $id;
   static $table;
 
-  //$routes['/register' => 'controller'=> 'user', 'method' => 'add'];
   public static function connect($url, $route)
   {
     self::$routes[$url] = $route;
@@ -25,7 +23,6 @@ class Router
         $arr[0] == 'user' && isset($arr[1]) && is_numeric($arr[1])
         || $arr[0] == 'user' && isset($arr[1]) && $arr[1] == '?'
       ) {
-        // Router::$id['col'] = 'id';
         Router::$id = $arr[1];
         $url = '/user/{id}';
       } else if ($arr[0] == 'user' && isset($arr[1]) && preg_match('/@/', $arr[1])) {
